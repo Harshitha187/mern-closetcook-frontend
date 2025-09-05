2852624184b010938478dac0d3031f2f23270e64
Harshitha187 <harshithayenigalla@gmail.com>
2025-09-05 21:21:53 +0530

    change

--- vite-project/src/components/header.jsx
+++ vite-project/src/components/header.jsx
@@ -7,9 +7,9 @@ const Header = () => {
   return (
     <div className='flex flex-col items-center px-4 text-center sm:px-4'>
       <img src="/src/assets/logo.png" alt="Logo" className="mb-4 w-40 h-40 sm:w-20 sm:h-20" />
-    <h1  className='text-3xl p-5'>Hey {userData?userData.name:'there!'}</h1>
-      <h1 className='text-4xl font-semibold mb-4 sm:text-5xl '>Welcome to ClosetCook</h1>
-      <h2 className='text-2xl text-gray-600 p-5 max-w-2xl mb-4'>Discover outfit recommendations tailored to your taste. Record your creations and explore styling options from a vibrant range of options.</h2>
+    <h1  className='text-3xl p-5 sm:text-2xl'>Hey {userData?userData.name:'there!'}</h1>
+      <h1 className='text-4xl font-semibold mb-4 sm:text-3xl '>Welcome to ClosetCook</h1>
+      <h2 className='text-2xl text-gray-600 p-5 max-w-2xl mb-4 sm:text-2xl'>Discover outfit recommendations tailored to your taste. Record your creations and explore styling options from a vibrant range of options.</h2>
     <button onClick={() => navigate('./test') } className='border border-gray-600 text-lg px-4 py-2 rounded-full hover:bg-gray-50'>Get started</button>
     </div>
   )
